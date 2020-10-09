d0e25110e323ebc027684ccf45175e366e179669
ak <thedeadlymortal@gmail.com>
2020-10-09 17:17:09 +0530

    add predict view

---
+++ backend/server/app/endpoints/urls.py
@@ -0,0 +1,24 @@
+# backend/server/apps/endpoints/urls.py file
+from django.conf.urls import url, include
+from rest_framework.routers import DefaultRouter
+
+from .views import EndpointViewSet
+from .views import MLAlgorithmViewSet
+from .views import MLAlgorithmStatusViewSet
+from .views import MLRequestViewSet
+from app.endpoints.views import PredictView
+
+
+router = DefaultRouter(trailing_slash=False)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+
+
+urlpatterns = [
+    url(r"^api/v1/", include(router.urls)),
+    url(
+        r"^api/v1/(?P<endpoint_name>.+)/predict$", PredictView.as_view(), name="predict"
+    ),
+]
